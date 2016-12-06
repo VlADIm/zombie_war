fab9ac8f08dfd1aa836e8a74b55e3917dce3afe3
jdiaz48 <justin529@hotmail.com>
2016-12-06 10:23:00 -0500

    Add files via upload

--- game.py
+++ game.py
@@ -1,29 +1,72 @@
 import json
+import house
+import pygame
+
+pygame.init()
+clock = pygame.time.Clock()
+
+
 
 class Game:
-    """docstring here"""
+    """
+    docstring
+    """
     def __init__(self, save):
         self.save = save
 
     def loadGame(self):
-        """docstring"""
-        json_str = json.loads(open("saves.json","r").read())
-        player_state_save = json_str.get(self.save)
+        """
+        Parameters: self (the objects name)
+        Function: loads data from the saves.json file and returns that data
+        """
+        json_dict = json.loads(open("saves.json","r").read())
+        if(self.save == 'all'):
+            player_state_save = list(json_dict.keys())
+
+        else:
+            player_state_save = json_dict.get(self.save)
+            player_state_save = {self.save : player_state_save}
 
         return player_state_save
 
 
     def saveGame(self, new_save):
-        """docstring"""
-        #opens entire file to read, records all file data, closes file
+        """
+        Parameters: self (the objects name), new_save (the game data that you want to save)
+        Function: Saves the new data to the saves.json without deleating other saves and overwrites the save of the same name if found
+        """
         read_json_file = open("saves.json","r")
         old_data = json.loads(read_json_file.read())
         read_json_file.close
 
-        #saves over old file data
         old_data[self.save] = new_save
 
-        #opens file to write, writes in new save data, closes file
         write_json_file = open("saves.json","w")
         json.dump(old_data, write_json_file, indent=4)
         write_json_file.close()
+
+    def upgrade(self, improvement):
+        """
+        docstrings
+        """
+        save_dict = self.loadGame()
+        info = save_dict.get(self)
+        value = info.get(improvement) + 1 #this needs to be fixed
+        self.saveGame(info)
+
+    def housePositions(self):
+        """
+        Parameters: self (the objects name)
+        Function: Opens the level_data.json file and returns the values of the houses as a list
+        """
+        houses = self.loadGame()
+        houses = houses.get(self.save)
+        json_dict = json.loads(open("level_data.json","r").read())
+        houses = json_dict.get("level_" + str(houses.get("level")))
+        print(houses)
+        house_list = []
+        for i in houses:
+            single_house = houses.get(i)
+            single_house = house.House(single_house.get('x'), single_house.get('y'), single_house.get('player_pop'), single_house.get('player_control'), single_house.get('connections'))
+            house_list.append(single_house)
+        return house_list
